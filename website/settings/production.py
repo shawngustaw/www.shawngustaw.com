9d1ada5976586f250ff589f304d39436b6f64746
Shawn Gustaw <self@shawngustaw.com>
2016-06-05 23:43:06 -0400

    update production settings

--- website/settings/production.py
+++ website/settings/production.py
@@ -2,9 +2,23 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
+SECRET_KEY = None
+ALLOWED_HOSTS = None 
+
 DEBUG = False
 
 try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'blog',
+		'USER': 'blogger',
+		'PASSWORD': None,
+		'HOST': 'localhost',
+		'PORT': '',
+	}
+}
